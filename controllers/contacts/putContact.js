68056ef8ac9d814362adbdc4766be7ffe08cad94
BartoszZygmunt <it@bartoszzygmunt.pl>
2024-02-04 15:10:45 +0100

    pagination

--- controllers/contacts/putContact.js
+++ controllers/contacts/putContact.js
@@ -1,34 +1,24 @@
 import Contact from "#models/contact.js";
 
 export const putContact = async (req, res, next) => {
+  const { params, body } = req;
+  const { contactId } = params;
+
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).json({
+      message: "missing fields",
+    });
+  }
+
   try {
-    const { params, body } = req;
-    const { contactId } = params;
-
-    if (Object.keys(req.body).length === 0) {
-      return res.status(400).json({
-        message: "missing fields",
-      });
-    }
-
-    const { error } = Contact.validate(req.body);
-
-    if (error) {
-      return res.status(400).json({
-        message: error.details[0].message,
-      });
-    }
-
     await Contact.findByIdAndUpdate(contactId, body);
 
     res.status(200).json({
       data: { contactId, ...body },
     });
   } catch (error) {
-    return res.status(404).json({
+    res.status(404).json({
       message: error.message,
     });
   }
 };
-
-//wygeneruj kod do logowania nr 1...
